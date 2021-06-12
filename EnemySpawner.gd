128f924fbee1952ac05e9117fb2d2b42b150c7e0
Johna <johnpeterkrause>
2021-06-12 09:19:42 +0200

    added export var to enemy spawner

--- EnemySpawner.gd
+++ EnemySpawner.gd
@@ -2,6 +2,10 @@ extends Node2D
 
 onready var enemy = preload("res://Enemy.tscn")
 var spawns = globals.current_level * 5
+export var spawn_interval = 2
+
+func _ready():
+	$InnerSpawner.wait_time = spawn_interval
 
 func _on_InnerSpawner_timeout():
 	var enemy_instance = enemy.instance()
